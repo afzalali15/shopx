4479a722a17fa6904a731ee0f0a2c4f545903d2f
Afzal Ali <afzalali15@gmail.com>
2021-02-13 18:09:35 +0400

    Update homepage.dart
    Removed hard coded colors

--- lib/views/homepage.dart
+++ lib/views/homepage.dart
@@ -12,19 +12,17 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.transparent,
         elevation: 0,
         leading: Icon(
           Icons.arrow_back_ios,
-          color: Colors.black,
         ),
         actions: [
           IconButton(
-              icon: Icon(
-                Icons.shopping_cart,
-                color: Colors.black,
-              ),
-              onPressed: () {})
+            icon: Icon(
+              Icons.shopping_cart,
+            ),
+            onPressed: () {},
+          )
         ],
       ),
       body: Column(
