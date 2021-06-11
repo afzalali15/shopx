ae68841c308d14b5c9479d0c6b48dd6eb6824978
Afzal Ali <afzalali15@gmail.com>
2021-06-11 21:59:37 +0400

    package upgrade

--- lib/services/remote_services.dart
+++ lib/services/remote_services.dart
@@ -5,8 +5,8 @@ class RemoteServices {
   static var client = http.Client();
 
   static Future<List<Product>> fetchProducts() async {
-    var response = await client.get(
-        'http://makeup-api.herokuapp.com/api/v1/products.json?brand=maybelline');
+    var response = await client.get(Uri.parse(
+        'https://makeup-api.herokuapp.com/api/v1/products.json?brand=maybelline'));
     if (response.statusCode == 200) {
       var jsonString = response.body;
       return productFromJson(jsonString);
--- lib/views/homepage.dart
+++ lib/views/homepage.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'package:get/get.dart';
 import 'package:get/instance_manager.dart';
-import 'package:shopx/controllers/productcontroller.dart';
+import 'package:shopx/controllers/product_controller.dart';
 import 'package:shopx/views/product_tile.dart';
 
 class HomePage extends StatelessWidget {
